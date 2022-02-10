eeac6f788a70fd10b31eee3072385418c783b5f5
Daniel Ocampo <ochampo77@gmail.com>
2022-02-09 20:44:00 -0800

    added pop_first,prepend, get methods to link list

--- DataStructures/LinkedList.ps1
+++ DataStructures/LinkedList.ps1
@@ -45,9 +45,8 @@ class LinkedList {
         
     }
 
-    pop()
+    [int]pop()
     {
-
         $temp = $this.head
         while($temp.next)
         {
@@ -57,21 +56,83 @@ class LinkedList {
         
         $this.tail = $this.pre 
         $this.tail.next = $null
-        Write-Host $temp.value
-
+        $this.length -= 1
+        return $temp.value
     }
-    
+
+    popFirst()
+    {
+     $temp =  $this.head
+     $this.head = $this.head.next
+     $temp.next = $null
+     $this.length -= 1
+    }
+
+    prePend($value)
+    {
+       $node = [Node]::new($value)
+       $node.next =  $this.head
+       $this.head = $node
+       $this.length += 1
+        write-host $node.value
+    }
+
+    IsEmptyStack()
+    {
+        if ($this.length -eq 0 )
+        {
+            $this.head = new_node
+            $this.tail = new_node
+        }
+    }
+
+      get($index)
+      {
+        if ($index -lt 0 -or $index -ge $this.length)
+        {
+            write-host "Out Of range"
+        }
+      
+        else
+        {
+            $temp = $this.head
+            $i=0
+            while( $i -lt $index )
+            {
+            $temp = $temp.next
+            $i++
+            }
+
+            Write-Host $temp.Value
+
+        }
+         
+        
+      }
 }
 
 $myLinkList = [LinkedList]::new(12)
 $myLinkList.append(15)
 $myLinkList.append(54)
 $myLinkList.append(60)
-$myLinkList.PrintList()
+$myLinkList.append(60)
+$myLinkList.append(30)
 $myLinkList.PrintList()
 
-write-Host "Pop:"
+write-Host "Pop test:"
 
 $myLinkList.pop()
-
+write-Host "NewList:"
+$myLinkList.PrintList()
+Write-Host "prepend Test"
+$myLinkList.prePend(50)
+write-Host "NewList:"
+$myLinkList.PrintList()
+write-host "Pop_first"
+$myLinkList.popFirst()
+write-Host "NewList:"
+$myLinkList.PrintList()
+write-host "get"
+$myLinkList.get(2)
+write-Host "NewList:"
 $myLinkList.PrintList()
