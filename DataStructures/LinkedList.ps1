5c41ffdad20ade9602cbb53dd0b2bd8b493234d9
Daniel Ocampo <ochampo77@gmail.com>
2022-02-13 03:16:58 -0800

    reverse LinkedList ( pain)

--- DataStructures/LinkedList.ps1
+++ DataStructures/LinkedList.ps1
@@ -15,7 +15,7 @@ class LinkedList {
     $head
     $tail
     $length = 0
-    $pre
+    
     LinkedList([int] $value)
    {
        $node = [Node]::new($value)
@@ -39,6 +39,7 @@ class LinkedList {
      append([int] $value)
      {
          $node = [Node]::new($value)
+        
          $this.tail.next = $node
          $this.tail = $node
          $this.length += 1
@@ -47,14 +48,16 @@ class LinkedList {
 
     [int]pop()
     {
+        
         $temp = $this.head
+        $pre = $this.head
         while($temp.next)
         {
-            $this.pre = $temp
+            $pre = $temp
             $temp = $temp.next
         }
         
-        $this.tail = $this.pre 
+        $this.tail = $pre 
         $this.tail.next = $null
         $this.length -= 1
         return $temp.value
@@ -104,8 +107,6 @@ class LinkedList {
             $i++
             }
 
-            
-
         }
         return $temp 
       }
@@ -117,35 +118,102 @@ class LinkedList {
         $temp.value = $value
     }
 
+    insert($index, $value)
+    {
+        $node = [Node]::new($value)
+        $temp = $this.get($index -1)
+        $node.next = $temp.next
+        $temp.next = $node
+        $this.length += 1 
+        $this.head
+    }
+
+    [psobject]remove($index)
+    {
+    $prev = $this.get($index-1)
+    $temp = $prev.next
+    $prev.next = $temp.next
+    $temp.next = $null
+    $this.length -= 1
+    return $temp
+ 
+    }
+
+    reverseLinkedList()
+    {
+     $temp = $this.head
+     $this.head = $this.tail 
+     $this.tail = $temp
+     $after = $temp.next
+     $before = $null
+     
+     for( $i = 0; $i -lt $this.length; $i++) 
+     {
+         $after  =  $temp.next
+         $temp.next = $before
+         $before = $temp
+         $temp = $after
+
+     }
+
+    
+
+    
+    }
 
 }
 
-$myLinkList = [LinkedList]::new(12)
-$myLinkList.append(15)
-$myLinkList.append(54)
-$myLinkList.append(60)
-$myLinkList.append(60)
-$myLinkList.append(30)
-$myLinkList.PrintList()
+ $myLinkList = [LinkedList]::new(0)
+ $myLinkList.append(1)
+ $myLinkList.append(2)
+ $myLinkList.append(3)
+ $myLinkList.append(4)
+ $myLinkList.append(5)
+ $myLinkList.PrintList()
+ Write-Host "test rev"
+ $myLinkList.reverseLinkedList()
+ $myLinkList.PrintList()
 
-write-Host "Pop test:"
+# $myLinkList = [LinkedList]::new(12)
+# $myLinkList.append(15)
+# $myLinkList.append(54)
+# $myLinkList.append(60)
+# $myLinkList.append(70)
+# $myLinkList.append(30)
+# $myLinkList.PrintList()
 
-$myLinkList.pop()
-write-Host "NewList:"
-$myLinkList.PrintList()
-Write-Host "prepend Test"
-$myLinkList.prePend(50)
-write-Host "NewList:"
-$myLinkList.PrintList()
-write-host "Pop_first"
-$myLinkList.popFirst()
-write-Host "NewList:"
-$myLinkList.PrintList()
-write-host "get"
-$myLinkList.get(2).Value
-write-Host "NewList:"
-$myLinkList.PrintList()
-write-host "Set Value based on index"
-$myLinkList.setValue(1,1)
-write-Host "NewList:"
-$myLinkList.PrintList()
+# write-Host "Pop test:"
+
+# $myLinkList.pop()
+# write-Host "NewList:"
+# $myLinkList.PrintList()
+# Write-Host "prepend Test"
+# $myLinkList.prePend(50)
+# write-Host "NewList:"
+# $myLinkList.PrintList()
+# write-host "Pop_first"
+# $myLinkList.popFirst()
+# write-Host "NewList:"
+# $myLinkList.PrintList()
+# write-host "get"
+# $myLinkList.get(2).Value
+# write-Host "NewList:"
+# $myLinkList.PrintList()
+# write-host "Set Value based on index"
+# $myLinkList.setValue(1,1)
+# write-Host "NewList:"
+# $myLinkList.PrintList()
+# write-host "insert Value based on index"
+# $myLinkList.insert(1,2)
+# write-Host "NewList:"
+# $myLinkList.PrintList()
+
+# write-host "remove Value based on index"
+# $myLinkList.remove(2).value
+# write-Host "NewList:"
+# $myLinkList.PrintList()
+
+# write-host "reverse LinkedList"
+# $myLinkList.reverseLinkedList()
+# write-Host "NewList:"
+# $myLinkList.PrintList()
