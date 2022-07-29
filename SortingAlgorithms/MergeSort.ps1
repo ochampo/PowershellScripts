ec04c4c437366083e585332d1e21a019ebadaad0
Daniel Ocampo <ochampo77@gmail.com>
2022-07-29 00:00:03 -0700

    added a swap function, added more logic to mergeS

--- SortingAlgorithms/MergeSort.ps1
+++ SortingAlgorithms/MergeSort.ps1
@@ -1,9 +1,29 @@
 function MergSort($data)
 {
+    if($data.Length -gt 1 )
+    {
     $midpoint = ($data).Length / 2
-
     $leftSide = $data[0 .. $midpoint]
     $rightSide = $data[($midpoint+1) .. $data.Length]
+
+
+    MergSort($leftSide)
+    MergSort($rightSide)
+    }
+
+$i = 0
+
+$j = 0
+ 
+$k = 0
+
+while ($i -lt $leftSide -and $j -lt $rightSide.Length)
+{
+
+
+
+
+}
     
     
     
