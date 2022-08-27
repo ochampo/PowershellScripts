4166f226d2fe326404ed4c3ea4a4bc4533ba5360
Daniel Ocampo <ochampo77@gmail.com>
2022-08-27 01:43:42 -0700

    fixed append method and print method

--- DataStructures/DoublyLinkedList.ps1
+++ DataStructures/DoublyLinkedList.ps1
@@ -8,28 +8,56 @@ Node($value)
     $this.value = $value
     $this.next = $null
     $this.prev = $null
-    $this.Length = 1
 }
 
 }
 
 
 class DoublyLinkedList{
+    $head
+    $tail
+    $length = 0
     
-    $newNode = [Node]::new($value)
-    $head = $newNode
-    $tail = $newNode
-    $length = 1
-
+    DoublyLinkedList([int] $value)
+   {
+       $node = [Node]::new($value)
+       $this.head = $node
+       $this.tail = $node
+       $this.length = 1 
+    }
+    
     [void] PrintList()
     {
         $temp = $this.head
         while($null -ne $temp)
         {
-            write-output ($temp.value)
+            write-host $temp.value
             $temp = $temp.next
         }
 
     }
 
+    [void]append($value)
+    {
+        $node = [Node]::new($value)
+        if($null -eq $this.head)
+        {
+            $this.head = $node
+            $this.tail = $node
+        }
+        else 
+        {
+            $this.tail.next = $node
+            $node.prev = $this.tail
+            $this.tail = $node
+        }
+
+        $this.length += 1
+    }
+
 }
+
+$doublyLinkedList = [DoublyLinkedList]::new(10)
+$doublyLinkedList.append(11)
+
+$doublyLinkedList.PrintList()
