21cc8b06ba430737cfb06d618ddf7ac5d0e0e2a9
Daniel Ocampo <ochampo77@gmail.com>
2022-09-13 21:57:02 -0700

    implemented a push method and a print method.

--- DataStructures/Stack.ps1
+++ DataStructures/Stack.ps1
@@ -2,7 +2,6 @@ class Node
 {
     $value
     $next
-    $prev
     Node($value)
     {
     $this.value = $Value
@@ -15,15 +14,52 @@ class Stack
 {
     $top
     $height = 0
+    Stack()
+    {
+    
+    }
     Stack($value)
     {
     $node = [Node]::new($value)
     $this.top = $node
     $this.height += 1
-    
+    }
+
+    [void] push($value)
+    {
+        $node = [Node]::new($value)
+        if ($this.height -eq 0)
+        {
+            $this.top = $node
+        }
+        else
+        {
+            $node.next = $this.top
+            $this.top = $node
+        }
+        $this.height += 1
+    }
+    [void] PrintStack()
+    {
+        $temp = $this.top
+        while($null -ne $temp)
+        {
+            write-host $temp.value
+            $temp = $temp.next
+            
+        }
+
+
     }
 
 
-
-
 }
+
+$stack = [Stack]::new()
+
+$stack.push(1)
+$stack.push(2)
+$stack.push(3)
+$stack.push(4)
+$stack.push(5)
+$stack.PrintStack()
