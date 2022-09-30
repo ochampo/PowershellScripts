28b82623d90bfcd6947cf9f8c0090827401d43ed
Daniel Ocampo <ochampo77@gmail.com>
2022-09-29 23:58:10 -0700

    added a node class with a print method.

--- DataStructures/Queue.ps1
+++ DataStructures/Queue.ps1
@@ -1,12 +1,37 @@
-Import-Module .\Node.ps1
+class Node
+{
+    $value
+    $next
+    Node($value)
+    {
+    $this.value = $Value
+    $this.next = $null
+    }
+}
+
 
 class Queue{
+ [psobject]$first 
+ [psobject]$last
+ [int]$length = 0 
 
-  Queue($value)
+ Queue($value)
   {
-
-
+    $node = [Node]::new($value)
+    $this.first = $node
+    $this.last = $node
+    $this.Length = 1
   }
 
+  [void] printQueue()
+  {
+    $temp = $this.first
+
+    while($null -ne $temp)
+    {
+      Write-host $temp.value
+      $temp = $temp.next
+    }
+  }
 
 }
