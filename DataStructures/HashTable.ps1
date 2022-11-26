a5e69ee87f95d27d9f44957c95b5f57c04f6089c
Daniel Ocampo <danocampo@meta.com>
2022-11-25 21:32:29 -0800

    created a get method

--- DataStructures/HashTable.ps1
+++ DataStructures/HashTable.ps1
@@ -26,6 +26,28 @@ $DataMap = ,$null * 7
     $this.DataMap[$index] += [pscustomobject]@{Key=$key;Value=$value}
 
 }
+[void] GetMethod($key)
+{
+    $index = $this.Hash($key)
+    if($null -ne $this.DataMap[$index])
+    {
+        for($i=0;$i -lt ($this.DataMap[$index]).length; $i++ )
+        {
+             if( $key -eq ($this.DataMap[$index])[$i].Key)
+             {
+                Write-host "Value is this " + ($this.DataMap[$index])[$i].Value
+             }
+        }
+    }
+    else {
+        write-host "no values found"
+       }
+   
+
+
+
+
+}
 
 [void] PrintTable()
 {
@@ -44,6 +66,17 @@ $myHash.SetMethod('Dan',11)
 $myHash.SetMethod('data',110)
 $myHash.SetMethod('bolts',1400)
 $myHash.SetMethod('lumber',100)
+$myHash.SetMethod('whatever',56)
+$myHash.SetMethod('homer',1400)
+$myHash.SetMethod('water',100)
+
+$myHash.GetMethod("Dan")
+$myHash.GetMethod("bolts")
+$myHash.GetMethod("Bolts")
+$myHash.GetMethod('whatever')
+$myHash.GetMethod('cool')
+$myHash.GetMethod('homer')
+$myHash.GetMethod('water')
 
 
 $myHash.PrintTable()
