a13151c3baaa02bda45d25aacda409438c08bc29
Daniel Ocampo <danocampo@meta.com>
2023-01-10 23:23:59 -0800

    started graph class. still working on logic to add

--- DataStructures/Graphs.ps1
+++ DataStructures/Graphs.ps1
@@ -1,3 +1,19 @@
 #graphs 
 # waited graphs - allow for routes based on the wieght 
 # Adjacency Matrix 
+
+class Graph
+{
+    $adjList = {}
+
+    [void] addVertex($vertex)
+    {
+        if($vertex.contains($this.adjList))
+        {
+            $this.adjList[$vertex] 
+        }
+
+    }
+    
+    
+}
